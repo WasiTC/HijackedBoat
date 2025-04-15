7a05c4486b4e5d33d41e53124cc89f9a1312f961
WasiTC <wasihossain692@gmail.com>
2025-04-15 16:50:39 +0600

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,33 @@
+local budget = 5000
+local totalcost = 250 + 300 + 325 + 400
+if budget < totalcost then return end
+
+local qty = mathxfloor(budget / totalcost)
+
+local args = {
+    [1] = Wood,
+    [2] = qty
+}
+
+workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
+task.wait(5)
+local args = {
+    [1] = Titanium,
+    [2] = qty
+}
+
+workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
+task.wait(5)
+local args = {
+    [1] = Plastic,
+    [2] = qty
+}
+
+workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
+task.wait(5)
+local args = {
+    [1] = Metal,
+    [2] = qty
+}
+
+workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
