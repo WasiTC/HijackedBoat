ff333f0c61f7405fb29b5559753d608cb1f44e02
WasiTC <wasihossain692@gmail.com>
2025-04-15 17:25:22 +0600

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,32 +1,16 @@
-local budget = 5000
-local totalcost = 1275
+local month = tonumber(os.date("%m")) -- %m = month (01-12)
+local day = tonumber(os.date("%d"))   -- %d = day (01-31)
+
+if month == 7 and day == 4 then
+    local budget = 5000
+local totalcost = 250 + 300 + 325 + 400
 if budget < totalcost then return end
 
 local qty = math.floor(budget / totalcost)
+local items = {"FireworkA", "FireworkB", "FireworkC", "FireworkD"}
 
 local args = {
-    [1] = "WoodBlock",
-    [2] = qty
-}
-
-workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
-task.wait(5)
-local args = {
-    [1] = "TitaniumBlock",
-    [2] = qty
-}
-
-workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
-task.wait(5)
-local args = {
-    [1] = "PlasticBlock",
-    [2] = qty
-}
-
-workspace:WaitForChild("ItemBoughtFromShop"):InvokeServer(unpack(args))
-task.wait(5)
-local args = {
-    [1] = "MetalBlock",
+    [1] = items,
     [2] = qty
 }
 
